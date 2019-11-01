d34479fd6a46bb75f09e0acc5401242c2351714a
Angel de Jesus Zepeda Torres <angel.zepeda0508@hotmail.com>
2019-11-01 13:17:41 -0600

    changes routes

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
 export default {
-    server: 'http://localhost:5000/api/v1/',
-    host: 'http://localhost:5000/'
+    server: 'http://157.245.12.180:5000/api/v1/',
+    host: 'http://157.245.12.180:5000/'
 }
